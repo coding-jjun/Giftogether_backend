2c2ea98b37108791ddba9a4fb35c79f9586b5ec5
ChoiWheatley <chltmdgus604@naver.com>
2024-12-26 01:07:42 +0900

    test(deposit.e2e): WIP for matched deposit

--- src/features/deposit/deposit.e2e.spec.ts
+++ src/features/deposit/deposit.e2e.spec.ts
@@ -21,6 +21,7 @@ import { Donation } from 'src/entities/donation.entity';
 import { FundTheme } from 'src/enums/fund-theme.enum';
 import { ProvisionalDonationStatus } from 'src/enums/provisional-donation-status.enum';
 import { CommonResponse } from 'src/interfaces/common-response.interface';
+import { DepositStatus } from 'src/enums/deposit-status.enum';
 
 const entities = [
   Deposit,
@@ -40,6 +41,7 @@ describe('Deposit API E2E Test', () => {
   let provDonRepo: Repository<ProvisionalDonation>;
   let userRepo: Repository<User>;
   let fundingRepo: Repository<Funding>;
+  let depositRepo: Repository<Deposit>;
   let mockFunding: Funding;
   let mockFundingOwner: User;
   let mockDonor: User;
@@ -59,6 +61,7 @@ describe('Deposit API E2E Test', () => {
     provDonRepo = moduleFixture.get(getRepositoryToken(ProvisionalDonation));
     userRepo = moduleFixture.get(getRepositoryToken(User));
     fundingRepo = moduleFixture.get(getRepositoryToken(Funding));
+    depositRepo = moduleFixture.get(getRepositoryToken(Deposit));
     g2gException = moduleFixture.get(GiftogetherExceptions);
     await app.init();
 
@@ -115,9 +118,10 @@ describe('Deposit API E2E Test', () => {
   describe('POST /deposits', () => {
     it('should handle matched deposit', async () => {
       // Create matching provisional donation
+      const senderSig = 'HONG-1234';
       const provDon = ProvisionalDonation.create(
         g2gException,
-        'HONG-1234',
+        senderSig,
         mockDonor,
         10000,
         mockFunding,
@@ -127,7 +131,7 @@ describe('Deposit API E2E Test', () => {
       await request(app.getHttpServer())
         .post('/deposits')
         .send({
-          senderSig: 'HONG-1234',
+          senderSig,
           receiver: 'GIFTOGETHER',
           amount: 10000,
           transferDate: new Date(),
@@ -137,12 +141,27 @@ describe('Deposit API E2E Test', () => {
         })
         .expect(201);
 
-      const foundProvDon = await provDonRepo.findOne({
-        where: { senderSig: 'HONG-1234' },
+      // Provisional Donation의 상태가 Approved이어야 합니다.
+      const foundProvDons = await provDonRepo.find({
+        where: { senderSig },
       });
-      expect(foundProvDon.status).toBe(
+      expect(foundProvDons.length).toBe(1);
+      expect(foundProvDons[0].status).toBe(
         ProvisionalDonationStatus.Approved.toString(),
       );
+
+      // 이체내역의 상태가 Matched이어야 합니다.
+      const foundDeposits = await depositRepo.find({
+        where: { senderSig },
+      });
+      expect(foundDeposits.length).toBe(1);
+      expect(foundDeposits[0].status).toBe(DepositStatus.Matched.toString());
+
+      // Donation 인스턴스가 생성되어야 합니다.
+
+      // Donor에게 Notification이 생성되어야 합니다.
+
+      // FundOwner에게 Notification이 생성되어야 합니다.
     });
 
     it('should handle unmatched deposit', async () => {
