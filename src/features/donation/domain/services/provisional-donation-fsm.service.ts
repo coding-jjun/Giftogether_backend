df46e85cda70865d62f88678b4a66509ce4d1b50
ChoiWheatley <chltmdgus604@naver.com>
2025-01-08 18:17:11 +0900

    feat(provdon.fsm): Add transitions

---
+++ src/features/donation/domain/services/provisional-donation-fsm.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { ProvisionalDonationStatus as S } from 'src/enums/provisional-donation-status.enum';
+import { IFsmService } from 'src/interfaces/fsm-service.interface';
+import { EventName, Transition } from 'src/interfaces/transition.interface';
+
+@Injectable()
+export class ProvisionalDonationFsmService implements IFsmService<S> {
+  private readonly transitions: Transition<S>[] = [
+    {
+      from: S.Pending,
+      to: S.Approved,
+      event: ProvisionalDonationApprovedEvent.name,
+    },
+    {
+      from: S.Pending,
+      to: S.Rejected,
+      event: ProvisionalDonationPartiallyMatchedEvent.name,
+    },
+    {
+      from: S.Pending,
+      to: S.Rejected,
+      event: ProvisionalDonationTimedOutEvent.name,
+    },
+    {
+      from: S.Approved,
+      to: S.Pending,
+      event: ProvisionalDonationMatchCancelledEvent.name,
+    },
+    {
+      from: S.Rejected,
+      to: S.Pending,
+      event: ProvisionalDonationMatchCancelledEvent.name,
+    },
+  ];
+
+  transition(current: S, event: EventName): S {
+    throw new Error('Method not implemented.');
+  }
+}
