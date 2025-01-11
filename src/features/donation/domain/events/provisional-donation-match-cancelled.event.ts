1593528daeed8f03d6cca6746b5aa3c7514a84e2
ChoiWheatley <chltmdgus604@gmail.com>
2025-01-11 17:15:16 +0900

    remove deprecated flag on prov-don-event

--- src/features/donation/domain/events/provisional-donation-match-cancelled.event.ts
+++ src/features/donation/domain/events/provisional-donation-match-cancelled.event.ts
@@ -1,8 +1,5 @@
 import { BaseEvent } from 'src/interfaces/event.interface';
 
-/**
- * @deprecated DepositCancelledEvent로 통합되었습니다.
- */
 export class ProvisionalDonationMatchCancelledEvent extends BaseEvent {
   constructor(public readonly senderSig: string) {
     super();
--- src/features/donation/domain/events/provisional-donation-partially-matched.event.ts
+++ src/features/donation/domain/events/provisional-donation-partially-matched.event.ts
@@ -1,8 +1,5 @@
 import { BaseEvent } from 'src/interfaces/event.interface';
 
-/**
- * @deprecated DepositPartiallyMatchedEvent로 통합되었습니다.
- */
 export class ProvisionalDonationPartiallyMatchedEvent extends BaseEvent {
   constructor(public readonly senderSig: string) {
     super();
